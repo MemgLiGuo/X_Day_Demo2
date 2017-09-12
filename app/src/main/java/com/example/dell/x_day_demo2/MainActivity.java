d18a83a493bce2d035ef1f66a598b53d507960e2
MemgLiGuo <2264280742@qq.com>
2017-09-12 09:30:39 +0800

    首次提交

---
+++ app/src/main/java/com/example/dell/x_day_demo2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.dell.x_day_demo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+        Log.e("TAG","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    }
+}
